5a81010ae7671e7e2fddffff8be893c2c39ca4a7
ganavijayaram <jayaram.g@northeastern.edu>
2023-08-08 13:01:29 -0400

    changing back to docker desktop context

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,10 @@ import express from "express";
 const router = express.Router()
 
 router.post('/api/users/signup', (req, res) => {
+  //instead of using if else to check if the right info has been provided
+  // we will use express validator to validate our incoming requests
+  // 
+  const {email, password} = req.body
   res.send("Ganavi got an internship and fulltime in signup!!!")
 })
 
