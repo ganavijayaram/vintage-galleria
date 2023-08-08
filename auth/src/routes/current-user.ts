696fb2dd674ce01796fce621a195f42fa4c3187c
ganavijayaram <jayaram.g@northeastern.edu>
2023-08-08 13:41:55 -0400

    Handling the validation error

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,15 @@
-import express from "express";
+//Request and Response are the types to validate the req, res in the routes
+import express, {Request, Response} from "express";
+
+//using the  express validator to validate the imcoming requests
+import { body, validationResult } from "express-validator";
 
 const router = express.Router()
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send("Ganavi got an internship and fulltime!!!")
+  res.send("currentuser(/api/users/currentuser): Ganavi got coop and full time")
 })
+
 
 //renaming it because we will have multiple routers
 export {router as currentUserRouter}
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,13 +1,43 @@
-import express from "express";
+//Request and Response are the types to validate the req, res in the routes
+import express, {Request, Response} from "express";
+
+//using the  express validator to validate the imcoming requests
+import { body, validationResult } from "express-validator";
 
 const router = express.Router()
-
+/*
 router.post('/api/users/signup', (req, res) => {
-  //instead of using if else to check if the right info has been provided
-  // we will use express validator to validate our incoming requests
-  // 
+  res.send({"message": "Ganavi got an internship and fulltime!!!"})
+})
+*/
+router.post('/api/users/signup',  [
+  //checking is the body contains email
+  body('email')
+    //built in function for express to check if it is an email
+    .isEmail()
+    // if none of the above conditions are passes, will throw error with the following message
+    .withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .isLength({min: 4, max: 20})
+    .withMessage('Password must be betweeb 4 and 20 characters')
+    //Typescript needs to know the tyoe of the arguments and return type of the function
+] , (req: Request, res: Response) => {
+
+  //before we procees, if there are any errors during the validation, we are going to get those errors
+  const errors = validationResult(req)
+
+  if(!errors.isEmpty()) {
+    //sending the errors as an array
+    return res.status(400).send(errors.array())
+  }
+
+
   const {email, password} = req.body
-  res.send("Ganavi got an internship and fulltime in signup!!!")
+
+  console.log("current-user(/api/users/signup):Creating user")
+  
+  res.send({"message": "Ganavi got an internship and fulltime!!!!!"})
 })
 
 //renaming it because we will have multiple routers
