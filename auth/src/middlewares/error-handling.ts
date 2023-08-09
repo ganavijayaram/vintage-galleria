b0fe879ed1b6c1da1681eb007dcbaf77455040bd
ganavijayaram <jayaram.g@northeastern.edu>
2023-08-09 09:34:57 -0400

    Differentiating the type of error

--- auth/src/middlewares/error-handling.ts
+++ auth/src/middlewares/error-handling.ts
@@ -1,10 +1,21 @@
 import { Request, Response, NextFunction } from "express"
+import { RequestValidationError } from "../errors/request-validation-error"
+import { DatabaseConnectionError } from "../errors/database-connetion-error"
+
 
 
 // our front end will be getting same error structure from all the different microservices.
 // so that there is consistency and front will be able to handle the errors in the same fashion
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log("Error!")
+  if(err instanceof RequestValidationError) {
+    console.log('Request Validation Error!!')
+  }
+  else if(err instanceof DatabaseConnectionError) {
+    console.log('Database Connection Error!!')
+  }
+  else {
+    console.log('Unknown Type Error!!')
+  }
   res.status(400).send({
     "message": err.message
   })
