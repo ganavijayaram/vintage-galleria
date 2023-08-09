701118ab9aee3a974a96628f9ac533a4a4b0c593
ganavijayaram <jayaram.g@northeastern.edu>
2023-08-09 12:01:14 -0400

    Handling routes which are not defined

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,7 @@ import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handling";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express()
 app.use(json())
@@ -15,10 +16,15 @@ app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
+
+//for all the routes which are not defines, we have defined custom not found error, which is then handled by the errorHandler
+app.all('*', () => {
+  throw new NotFoundError()
+})
+
 //this has to be in the end, if there are any errors in the above routes,
 // it will come down through the stack of middlewares and execute appropriately
 app.use(errorHandler)
-
 
 //Listenig to port 
 app.listen(3000, () => {
--- auth/src/middlewares/error-handling.ts
+++ auth/src/middlewares/error-handling.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from "express"
-import { RequestValidationError } from "../errors/request-validation-error"
-import { DatabaseConnectionError } from "../errors/database-connetion-error"
 import { CustomError } from "../errors/custom-error"
+
 
 
 // our front end will be getting same error structure from all the different microservices.
