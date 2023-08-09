0ef81c6de99e0e4cd46b825cbc6ced7453ef3353
ganavijayaram <jayaram.g@northeastern.edu>
2023-08-09 10:03:21 -0400

    making sure all errors have the same structure when sending it to our front end

--- auth/src/middlewares/error-handling.ts
+++ auth/src/middlewares/error-handling.ts
@@ -9,12 +9,20 @@ import { DatabaseConnectionError } from "../errors/database-connetion-error"
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if(err instanceof RequestValidationError) {
     console.log('Request Validation Error!!')
+    const formattedError = err.errors.map((error) => {
+      if(error.type === 'field')
+      return {message: error.msg, field: error.path}
+    })
+    return res.status(400).send({errors: formattedError})
   }
+
   else if(err instanceof DatabaseConnectionError) {
     console.log('Database Connection Error!!')
+    return res.status(500).send({errors: [{message: err.reason}]})
   }
   else {
     console.log('Unknown Type Error!!')
+    return res.status(400).send({errors: [{message: 'Unknown Type Error!!'}]})
   }
   res.status(400).send({
     "message": err.message
