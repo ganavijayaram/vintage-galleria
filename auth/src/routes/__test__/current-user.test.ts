6206f364c21a850b6ee2adc7fdfa178a9b44ca16
ganavijayaram <jayaram.g@northeastern.edu>
2023-08-23 15:46:01 -0400

    abstraction of common code in the current-user

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -6,10 +6,8 @@ import { app } from "../../app";
 
 it('Responds with details of the current user', async() => {
   
-
     const cookie = await global.signin()
 
-    
     const response = await request(app)
     .get('/api/users/currentuser')
     // cookie will not be included in the follow up request as how it used to happen 
@@ -18,8 +16,23 @@ it('Responds with details of the current user', async() => {
     .send()
     .expect(200)
 
-    
 
    expect(response.body.currentUser.email).toEqual('ganavi1@gmail.com')
+
+})
+
+it('Responds with null if not authenticated', async() => {
+  
+  const cookie = await global.signin() 
+
+  const response = await request(app)
+  .get('/api/users/currentuser')
+  // cookie will not be included in the follow up request as how it used to happen 
+  // when requests were made with browser and postman
+  .send()
+  .expect(401) //CHECK THIS
+
+
+ expect(response.body.currentUser).toEqual(undefined)
 
 })
