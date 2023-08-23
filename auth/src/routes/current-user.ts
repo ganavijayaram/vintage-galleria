23bfa0427c7dc4d188b82092d7ede0a38462a473
ganavijayaram <jayaram.g@northeastern.edu>
2023-08-22 20:00:22 -0400

    Abstracting logic of loggedin user and JWT in current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -3,23 +3,14 @@ import express, {Request, Response} from "express";
 import jwt from 'jsonwebtoken'
 //using the  express validator to validate the imcoming requests
 import { body, validationResult } from "express-validator";
+import { currentUser } from "../middlewares/current-user";
+import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', (req, res) => {
+router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
 
-  //If the user is logged in we will get a request with JWT in the cookie, else empty if they user is loggin in 
-  //if(!req.session?.jwt) -> this is an alternative way
-  if(!req.session || !req.session.jwt) {
-    return res.send({currentUser: null})
-  }
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
-    res.send({currentUser: payload})
-  }
-  catch(err) {
-    res.send({currentUser: null})
-  }
+ res.send({currentUser: req.currentUser || null})
 
   
 })
