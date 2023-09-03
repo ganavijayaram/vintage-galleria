e4dba817e4b85e6d9231deb064473eadaad0f1b5
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-03 10:35:01 -0400

    Order service Creation and ENUM for order status

---
+++ version1.1/orders/src/models/orders.ts
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose'
+
+// properties which are needed to create an order
+interface orderAttrs {
+  userId: string // this is TS 
+  status: string
+  expiresAt: Date
+  artifact: ArtifactDoc
+}
+
+//properties which are end up in an order
+// sometimes we will have mongoose put additional properties apart fro the one what we use for creating an order
+interface orderDoc extends mongoose.Document{
+    userId: string
+    status: string
+    expiresAt: Date
+    artifact: ArtifactDoc
+}
+
+interface orderModel extends mongoose.Model<orderDoc> {
+  // takes orderAttrs and gives of type orderDoc
+  build(attrs: orderAttrs): orderDoc
+
+}
+
+
+const orderSchema =  new mongoose.Schema({
+  userId: {
+    type: String, // this is JS datatype
+    required: true
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  expiresAt: {
+    type: mongoose.Schema.Types.Date
+  },
+  artifact: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Artifact'
+  }
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id
+      delete ret._id
+    }
+  }
+})
+
+orderSchema.statics.build = (attrs: orderAttrs) => {
+  return new Order(attrs)
+}
+
+const Order = mongoose.model<orderDoc, orderModel>('Order', orderSchema)
+
+export {Order}
--- version1.1/orders/src/routes/new.ts
+++ version1.1/orders/src/routes/new.ts
@@ -1,8 +1,26 @@
+// this is for people who are trying to purchase the artifact
+import { requireAuth, validateRequest } from '@vintagegalleria/common'
 import express, {Request, Response} from 'express'
+import { body } from 'express-validator'
+import mongoose from 'mongoose'
 
 const router = express.Router()
 
-router.post('/api/orders/', async (req: Request, res: Response) => {
+router.post('/api/orders/',
+ requireAuth,
+ [
+  body('artifactId')
+    .not()
+    .isEmpty()
+    // we are making sure that artifactId is mongoose verified Id, as we are 
+    // storing the Id in the mongoose
+    // but this will cause coupling, because right now we are usig the 
+    // mongo to store, but what if later we are using someother database
+    .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+    .withMessage('ArtifactId has to be Present')
+ ], 
+ validateRequest, 
+ async (req: Request, res: Response) => {
   res.send({"message": "Everything good is happening to Ganavi!"})
 })
 
