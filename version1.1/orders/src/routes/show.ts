dc3010091b24bdedbb4be1f64b2053d633a72baf
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-03 15:24:53 -0400

    Getting individual orders for a user

--- version1.1/orders/src/routes/show.ts
+++ version1.1/orders/src/routes/show.ts
@@ -1,9 +1,25 @@
 import express, {Request, Response} from 'express'
 
+import { NotAuthorizedError, NotFoundError, requireAuth } from '@vintagegalleria/common'
+import { Order } from '../models/orders'
+
 const router = express.Router()
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({"message": "Everything good is happening to Ganavi!"})
+router.get('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+
+  // populate is used to get the associate artifact for the order fetched
+  const order = await Order.findById(req.params.orderId).populate('artifact')
+
+  if(!order) {
+    throw new NotFoundError()
+  }
+
+  // if the user is not authorised to access the order
+  if(order.userId != req.currentUser!.id) {
+    throw new NotAuthorizedError()
+  }
+
+  res.send(order)
 })
 
 export {router as showOrderRouter}
