bdaf70bf231065293e486c17d33da46133c39367
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-03 15:48:14 -0400

    Testing fetching of individual orders for a user

---
+++ version1.1/orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,64 @@
+import { OrderStatus } from '@vintagegalleria/common'
+import request from 'supertest'
+
+import { app } from '../../app' 
+import { Artifact } from '../../models/artifact'
+import { Order } from '../../models/orders'
+
+it('fetches the order', async () => {
+  // create artifact
+  const artifactOne = await Artifact.build({
+    title: 'Vase',
+    price: 200
+  })
+
+await artifactOne.save()
+
+//build order with artifact
+const user = global.signin()
+const {body: order} = await request(app)
+  .post('/api/orders')
+  .set('Cookie', user)
+  .send({artifactId: artifactOne.id})
+  .expect(201)
+
+//fetch the order
+const {body: fetchorder} = await request(app)
+  .get(`/api/orders/${order.id}`)
+  .set('Cookie', user)
+  .send()
+  .expect(200)
+
+expect(fetchorder.id).toEqual(order.id)
+
+})
+
+
+it('returns error if accesising someone elses order', async () => {
+  // create artifact
+  const artifactOne = await Artifact.build({
+    title: 'Vase',
+    price: 200
+  })
+
+await artifactOne.save()
+
+//build order with artifact
+const userOne = global.signin()
+const userTwo = global.signin()
+const {body: order} = await request(app)
+  .post('/api/orders')
+  .set('Cookie', userOne)
+  .send({artifactId: artifactOne.id})
+  .expect(201)
+
+//fetch the order
+const {body: fetchorder} = await request(app)
+  .get(`/api/orders/${order.id}`)
+  .set('Cookie', userTwo)
+  .send()
+  .expect(401)
+
+
+
+})
