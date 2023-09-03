1d4647cc864446356bdcfc93fdfe0c0f7c9a361c
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-03 15:53:34 -0400

    Cancelling the order

--- version1.1/orders/src/routes/delete.ts
+++ version1.1/orders/src/routes/delete.ts
@@ -1,9 +1,31 @@
+import { NotAuthorizedError, NotFoundError, OrderStatus, requireAuth } from '@vintagegalleria/common'
 import express, {Request, Response} from 'express'
+import { Order } from '../models/orders'
 
 const router = express.Router()
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({"message": "Everything good is happening to Ganavi!"})
+
+// NOT actually deleting it instead we are cancelling it
+router.delete('/api/orders/delete', requireAuth, async (req: Request, res: Response) => {
+
+  
+  const order = await Order.findById(req.params.orderId)
+
+  if(!order) {
+    throw new NotFoundError()
+  }
+
+  // if the user is not authorised to access the order
+  if(order.userId != req.currentUser!.id) {
+    throw new NotAuthorizedError()
+  }
+
+  order.status = OrderStatus.Cancelled
+  await order.save()
+
+  res.status(204).send(order)
+
+ 
 })
 
 export {router as deleteOrderRouter}
