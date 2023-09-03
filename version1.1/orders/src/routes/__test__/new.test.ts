9f042be2961ffe501ba4269c6a9c55cba98816f6
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-03 14:50:17 -0400

    Testing creation of order

---
+++ version1.1/orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,66 @@
+import request from "supertest"
+import mongoose from "mongoose"
+
+import  {app} from '../../app'
+import { Order, OrderStatus } from "../../models/orders"
+import { Artifact } from "../../models/artifact"
+
+it('returns error id artifact does not exist', async () => {
+  // generating valid mongoose id
+  const artifactId =  new mongoose.Types.ObjectId()
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({artifactId})
+    .expect(404)
+})
+
+it('returns error if the artifact is reserved', async () => {
+
+  // creating artifact and saving in the DB
+  const artifact = Artifact.build({
+    title: 'Vase',
+    price: 200
+  })
+  await artifact.save()
+
+  // creating order and associating it with the above artifact
+  const order = Order.build({
+    userId: 'abcdef',
+    artifact: artifact,
+    status: OrderStatus.Created,
+    expiresAt: new Date()
+  })
+
+  await order.save()
+
+  // actual testing
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({artifactId: artifact.id})
+    .expect(400)
+  
+})
+
+it('reserves an artifact', async () => {
+  // creating artifact and saving in the DB
+  const artifact = Artifact.build({
+    title: 'Vase',
+    price: 200
+  })
+  await artifact.save()
+
+  
+  // actual testing
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({artifactId: artifact.id})
+    .expect(201)
+  
+  
+})
+
+it.todo('emits an order event')
