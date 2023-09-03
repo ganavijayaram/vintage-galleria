f9660dfda6a9e6dbeb462126a9a62556a4f51395
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-03 18:47:25 -0400

    Tetsing the publishing of order events

--- version1.1/orders/src/routes/__test__/delete.test.ts
+++ version1.1/orders/src/routes/__test__/delete.test.ts
@@ -3,6 +3,7 @@ import request from 'supertest'
 import { app } from '../../app'
 import { Artifact } from '../../models/artifact'
 import { Order, OrderStatus } from '../../models/orders'
+import { natsWrapper } from '../../nats-wrapper'
 
 
 it('marks order as cancelled', async() => {
@@ -38,4 +39,33 @@ it('marks order as cancelled', async() => {
 
 })
 
-it.todo('Emits a order cancelled event')
+it('Emits a order cancelled event', async () => {
+  // create artifact
+  const artifact = Artifact.build({
+    title: 'Vase',
+    price: 200
+  })
+
+  await artifact.save()
+
+  //make request to create order
+  const user = global.signin()
+  const {body: order} = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({artifactId: artifact.id})
+    .expect(201)
+
+    console.log(order)
+
+  //make request to cancel order
+  const cancelledOrder = await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204)
+
+ 
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
--- version1.1/orders/src/routes/__test__/new.test.ts
+++ version1.1/orders/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose"
 import  {app} from '../../app'
 import { Order, OrderStatus } from "../../models/orders"
 import { Artifact } from "../../models/artifact"
+import { natsWrapper } from "../../nats-wrapper"
 
 it('returns error id artifact does not exist', async () => {
   // generating valid mongoose id
@@ -63,4 +64,23 @@ it('reserves an artifact', async () => {
   
 })
 
-it.todo('emits an order event')
+// we are not using the real natswrapper and we are using the mock one
+it('emits an order event', async () => {
+   // creating artifact and saving in the DB
+   const artifact = Artifact.build({
+    title: 'Vase',
+    price: 200
+  })
+  await artifact.save()
+
+  
+  // actual testing
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({artifactId: artifact.id})
+    .expect(201)
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
+
+})
