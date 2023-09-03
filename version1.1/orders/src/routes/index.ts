2ccf46f246bc33a297d9ece72ff428e151a0cf7f
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-03 15:00:11 -0400

    Getting orders for a particular user

--- version1.1/orders/src/routes/index.ts
+++ version1.1/orders/src/routes/index.ts
@@ -1,9 +1,20 @@
 import express, {Request, Response} from 'express'
 
+//We are getting all the order associated with the user who is making the request
+import { requireAuth } from '@vintagegalleria/common'
+import { Order } from '../models/orders'
+
 const router = express.Router()
 
-router.get('/api/orders/delete', async (req: Request, res: Response) => {
-  res.send({"message": "Everything good is happening to Ganavi!"})
+router.get('/api/orders/', 
+requireAuth, 
+async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    // we get the current User details in the requireAuth
+    userId: req.currentUser!.id
+  }).populate('artifact')
+
+  res.send(orders)
 })
 
 export {router as indexOrderRouter}
