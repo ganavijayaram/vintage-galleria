d912943852140cb95da99322f9bdc7dcf7cc50d6
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-05 16:48:33 -0400

    Listening to expiration complete in the order service

---
+++ version1.1/orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,43 @@
+import { ExpirationCompleteEvent, Listener, OrderStatus, Subjects } from "@vintagegalleria/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/orders";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
+import { queueGroupName } from "./queue-group-name";
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete
+  queueGroupName = queueGroupName;
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+      // Get the order
+      const order = await Order.findById(data.orderId)
+
+      if(!order) {
+        throw new Error('Order not Found')
+      }
+
+      // After receiveing the event, we will change the order status as cancelled
+      order.set({
+        status: OrderStatus.Cancelled
+        // because then later for all the cancelled orders we wont know what artiafct was assocuited with it
+        // also in our order, we know if the order is in cancelled state then it means the artifact is not reserved
+        // artifact: null 
+      })
+
+      await order.save()
+
+
+      // Once we cancel we need to publish it 
+      await new OrderCancelledPublisher(this.client).publish({
+        id: order.id,
+        version: order.version,
+        artifact: {
+            id: order.artifact.id
+        }
+      })
+
+      msg.ack()
+
+  }
+  
+  
+}
