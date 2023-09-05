1c366e13ee8a6c31e9073fcb816a90fb8100d79b
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-04 23:45:54 -0400

    Testing listeners of Artifact service

---
+++ version1.1/artifacts/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,59 @@
+import { OrderCreatedEvent, OrderStatus } from "@vintagegalleria/common"
+import mongoose from "mongoose"
+import { Message } from "node-nats-streaming"
+import { Artifact } from "../../../models/artifact"
+import { natsWrapper } from "../../../nats-wrapper"
+import { OrderCreatedListener } from "../order-created-listener"
+
+const setup = async () => {
+  // Create instance of client
+  const listener = new OrderCreatedListener(natsWrapper.client)
+
+  // Create and save the artifact
+  const artifact = Artifact.build({
+    title: 'Vase',
+    price: 200,
+    userId: 'abcdef'
+  })
+  await artifact.save()
+
+  // create fake data
+  const data: OrderCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    status: OrderStatus.Created,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    expiresAt: 'abcd',
+    artifact: {
+        id: artifact.id,
+        price: artifact.price
+    }
+  }
+
+  // create fake message
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn()
+  }
+
+  //return
+  return {msg, data, artifact, listener}
+}
+
+it('Sets the userId of the artifact', async () => {
+const {msg, data, artifact, listener} = await setup()
+
+await listener.onMessage(data, msg)
+
+const updatedArtifact = await Artifact.findById(artifact.id)
+
+expect(updatedArtifact!.orderId).toEqual(data.id)
+})
+
+it('acks the message', async () => {
+  const {msg, data, artifact, listener} = await setup()
+
+  await listener.onMessage(data, msg)
+  expect(msg.ack).toHaveBeenCalled()
+  
+})
