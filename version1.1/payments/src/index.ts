74b770294bfe4d61c03e1b4e3057947a6ad0022f
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-06 10:22:56 -0400

    Adding listeners in index file of Payment service

--- version1.1/payments/src/index.ts
+++ version1.1/payments/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import { natsWrapper } from './nats-wrapper';
 
 import { app } from './app';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 
 const start = async () => {
@@ -37,7 +39,8 @@ const start = async () => {
     process.on('SIGTERM', () => natsWrapper.client.close())
 
   
-
+    new OrderCreatedListener(natsWrapper.client).listen()
+    new OrderCancelledListener(natsWrapper.client).listen()
 
     await mongoose.connect(process.env.MONGO_URI);
 
